94c7d83530068c0aca68b9a588ab34374d9eea67
devLaolu <105947547+Laoluokiki@users.noreply.github.com>
2023-10-27 17:02:53 +0100

    Update urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,4 +1,6 @@
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from books.views import BookViewSet
 
@@ -7,4 +9,4 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
