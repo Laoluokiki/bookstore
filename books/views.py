192422b4a6e62573dbf92fe5d58c2d49e11108f1
Olaoluwa Okikiade <devlaolu>
2023-10-31 18:47:46 +0100

    local change

--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from .models import Book
-from .serializers import BookSerializer
+from .serializers import BookSerializer 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
