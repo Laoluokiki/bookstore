21c80a9470fb8c159e58b4efba8c18b320d20287
Olaoluwa Okikiade <devlaolu>
2023-10-31 15:08:24 +0100

    update on book model

--- books/models.py
+++ books/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_date = models.DateField()
-    isbn = models.CharField(max_length=20)
-    summary = models.TextField()
+    title: models.CharField = models.CharField(max_length=200)
+    author: models.CharField = models.CharField(max_length=100)
+    publication_date: models.DateField = models.DateField()
+    isbn: models.CharField = models.CharField(max_length=20)
+    summary: models.TextField = models.TextField()
