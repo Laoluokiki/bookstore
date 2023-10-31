690521b58c775ad56d48bbc779b3b507eff9a4ee
Olaoluwa Okikiade <devlaolu>
2023-10-31 16:46:51 +0100

    correct local eeror view

--- books/views.py
+++ books/views.py
@@ -3,5 +3,5 @@ from .models import Book
 from .serializers import BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all()/097766
     serializer_class = BookSerializer
