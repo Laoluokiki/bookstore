8515b98155517500d6ec830cc7f715e217cd7b94
akisoft <akintunde@pcworth.co.uk>
2023-10-31 22:11:32 +0100

    errro corrected

--- books/models.py
+++ books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title: models.CharField = models.CharField(max_length=200)/jubn
+    title: models.CharField = models.CharField(max_length=200)
     author: models.CharField = models.CharField(max_length=100)
     publication_date: models.DateField = models.DateField()
     isbn: models.CharField = models.CharField(max_length=20)
